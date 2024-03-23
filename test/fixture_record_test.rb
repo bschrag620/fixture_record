98d548a5c702d6cb98d889259c52f2dbe92d495c
Brad Schrag <brad.schrag@gmail.com>
2024-03-23 14:01:56 -0600

    basic test

--- test/fixture_record_test.rb
+++ test/fixture_record_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class FixtureRecordTest < ActiveSupport::TestCase
+  def yml_contents(klass)
+    YAML.load_file(Rails.root.join('test/fixtures', klass.table_name + '.yml'))
+  end
+
   test "it has a version number" do
     assert FixtureRecord::VERSION
+  end
+
+  test "it will write a record to the test fixture file" do
+    user = User.first
+    with_fixture_file_reset(User) do
+      refute yml_contents(User).key? user.test_fixture_name
+      user.to_test_fixture
+      assert yml_contents(User).key? user.test_fixture_name
+    end
+  end
+
+  test 'it will user the prefix and suffix values when supplied' do
+    user = User.first
+    user.fixture_record_prefix = :foo
+    user.fixture_record_suffix = :bar
+    assert user.test_fixture_name.start_with?('foo')
+    assert user.test_fixture_name.end_with?('bar')
+    with_fixture_file_reset(User) do
+      refute yml_contents(User).key? user.test_fixture_name
+      user.to_test_fixture
+      assert yml_contents(User).key? user.test_fixture_name
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,3 +12,25 @@ if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
   ActiveSupport::TestCase.file_fixture_path = File.expand_path("fixtures", __dir__) + "/files"
   ActiveSupport::TestCase.fixtures :all
 end
+
+
+def with_fixture_file_reset(*klasses)
+  original_data = klasses.to_h do |klass|
+    data = YAML.load_file(fixture_path_for(klass)) if File.exist?(fixture_path_for(klass))
+    [klass, data]
+  end
+
+  yield
+
+  original_data.each do |klass, data|
+    if data.nil?
+      File.delete(fixture_path_for_klass)
+    else
+      File.open(fixture_path_for(klass), 'w') { |f| f.write data.to_yaml }
+    end
+  end
+end
+
+def fixture_path_for(klass)
+  Rails.root.join('test/fixtures', klass.table_name + '.yml')
+end
